309f227b552b19acda7385ab37d3317f4dcbca5d
ShiJh⭐ <nnocreater@hotmail.com>
2022-09-16 08:19:40 +0000

    feat: metaserver's peers info persist

---
+++ metaserver/internal/usecase/logic/peers_logic.go
@@ -0,0 +1,50 @@
+package logic
+
+import (
+	"common/util"
+	"context"
+	"fmt"
+	"metaserver/internal/entity"
+	"metaserver/internal/usecase/pool"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+type Peers struct{}
+
+func NewPeers() Peers {
+	return Peers{}
+}
+
+func (Peers) GetPeers() ([]*entity.PeerInfo, error) {
+	key := fmt.Sprintf("peers_info/%s/", pool.Config.Cluster.GroupID)
+	res, err := pool.Etcd.Get(context.Background(), key, clientv3.WithPrefix())
+	if err != nil {
+		return nil, err
+	}
+	infoList := make([]*entity.PeerInfo, 0, len(res.Kvs))
+	for _, kv := range res.Kvs {
+		var info entity.PeerInfo
+		if err = util.DecodeMsgp(&info, kv.Value); err != nil {
+			return nil, err
+		}
+		infoList = append(infoList, &info)
+	}
+	return infoList, nil
+}
+
+func (Peers) RegisterSelf() error {
+	key := fmt.Sprintf("peers_info/%s/%s", pool.Config.Cluster.GroupID, pool.Config.Cluster.ID)
+	info := &entity.PeerInfo{
+		Location: util.GetHost(),
+		HttpPort: pool.Config.Port,
+		GrpcPort: pool.Config.Cluster.Port,
+		GroupID:  pool.Config.Cluster.GroupID,
+	}
+	bt, err := util.EncodeMsgp(info)
+	if err != nil {
+		return err
+	}
+	_, err = pool.Etcd.Put(context.Background(), key, string(bt))
+	return err
+}
