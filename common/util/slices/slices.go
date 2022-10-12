375da957ace8d02b35d8ed26dfb40f6c7624e650
ShiJh⭐ <nnocreater@hotmail.com>
2022-10-12 04:13:13 +0000

    feat: slices util

--- common/util/slices/slices.go
+++ common/util/slices/slices.go
@@ -1,5 +1,7 @@
 package slices
 
+import "common/util/math"
+
 func StringsReplace(arr []string, origin string, target string) bool {
 	for i, a := range arr {
 		if a == origin {
@@ -10,17 +12,26 @@ func StringsReplace(arr []string, origin string, target string) bool {
 	return false
 }
 
+// SafeChunk [start, end], negative number means counting from tail to head
 func SafeChunk[T any](arr []T, start, end int) []T {
-	if start < 0 && end < 0 {
-		return arr
-	} else if start < 0 {
-		return arr[:end]
-	} else if end < 0 {
-		return arr[start:]
+	// a mod b = a mod (-b)
+	// a mod b = - (-a mod b)
+
+	if start < 0 {
+		start = math.LogicMod(start, len(arr))
 	} else if start >= len(arr) {
-		return arr[0:0]
-	} else if end > len(arr) {
-		return arr[start:]
+		start = len(arr) - 1
 	}
-	return arr[start:end]
+
+	if end < 0 {
+		end = math.LogicMod(end, len(arr))
+	} else if end >= len(arr) {
+		end = len(arr) - 1
+	}
+
+	if start > end {
+		start, end = end, start
+	}
+
+	return arr[start : end+1]
 }
