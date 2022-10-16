8d317636632fde2590f89104d9584598f86ab3b7
Shijh <a838239178@gmail.com>
2022-10-16 12:42:34 +0800

    build: sbin/build.cmd

--- sbin/build.cmd
+++ sbin/build.cmd
@@ -1,5 +1,4 @@
-cd %~dp0..\src
-go build -o %~dp0..\bin\api.exe apiserver\main.go
-go build -o %~dp0..\bin\meta.exe metaserver\main.go
-go build -o %~dp0..\bin\object.exe objectserver\main.go
-go build -o %~dp0..\bin\admin.exe adminserver\main.go
+go build -o %~dp0..\bin\api.exe %~dp0..\src\apiserver\main.go
+go build -o %~dp0..\bin\meta.exe %~dp0..\src\metaserver\main.go
+go build -o %~dp0..\bin\object.exe %~dp0..\src\objectserver\main.go
+go build -o %~dp0..\bin\admin.exe %~dp0..\src\adminserver\main.go
