58db5f4c96c0467ce799c4efedbe5034d75a12d0
ShiJh⭐ <a838239178@gmail.com>
2022-10-25 03:44:06 +0000

    feat: server-stat api on admin-server

--- src/adminserver/internal/entity/server_state.go
+++ src/adminserver/internal/entity/server_state.go
@@ -5,9 +5,9 @@ import (
 )
 
 type ServerInfo struct {
-	ServerID string      `json:"serverId"`
-	HttpAddr string      `json:"httpAddr"`
-	RpcAddr  string      `json:"rpcAddr"`
-	SysInfo  system.Info `json:"sysInfo"`
-	IsMaster bool        `json:"isMaster"`
+	ServerID string       `json:"serverId"`
+	HttpAddr string       `json:"httpAddr"`
+	RpcAddr  string       `json:"rpcAddr"`
+	SysInfo  *system.Info `json:"sysInfo"`
+	// IsMaster bool        `json:"isMaster"`
 }
