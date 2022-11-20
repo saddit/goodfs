8a756c715eff0d450e26fc0c29fda70cc770e243
Shijh <a838239178@gmail.com>
2022-11-20 14:50:59 +0800

    feat(ui): mem size on server-card

--- src/adminserver/ui/src/components/ServerCard.vue
+++ src/adminserver/ui/src/components/ServerCard.vue
@@ -1,21 +1,22 @@
 <template>
-  <div class="relative min-w-40 py-3 px-2 bg-white h-40 w-40 md:h-44 md:w-44 shadow-lg rounded">
-    <span v-if="info.isMaster" class="absolute -top-2 -left-5 font-bold text-red-500">
-      Master
-    </span>
+  <div :class="{'outline outline-indigo-400 outline-offset-4': info.isMaster}"
+       class="relative min-w-40 py-3 px-2 bg-white h-48 w-40 md:h-44 md:w-44 shadow-lg rounded">
     <div class="inline-flex justify-between items-center px-2 w-full">
-      <ServerIcon class="text-indigo-600 w-10 h-10" />
+      <ServerIcon class="text-indigo-600 w-10 h-10"/>
       <span class="font-bold text-gray-900 text-lg">{{ info.serverId }}</span>
     </div>
-    <div class="inline-flex items-center w-fit py-2">
-      <CpuChipIcon class="w-8 h-8 mx-1 text-indigo-600" />
+    <div class="inline-flex items-center w-fit py-2 text-gray-500 text-xs">
+      <CpuChipIcon class="w-8 h-8 mx-1 text-indigo-600"/>
       <div>
-        <div class="text-gray-500 text-xs">{{ $t('core') }}: {{
+        <div>{{ $t('core') }}: {{
             info.sysInfo.cpuStatus.physicalCount
-        }}</div>
-        <div class="text-gray-500 text-xs">{{ $t('thread') }}: {{
+          }}
+        </div>
+        <div>{{ $t('thread') }}: {{
             info.sysInfo.cpuStatus.logicalCount
-        }}</div>
+          }}
+        </div>
+        <div>{{ $t('ram') }}: {{ $utils.formatBytes(info.sysInfo.memStatus.all) }}</div>
       </div>
     </div>
     <div class="space-y-1 absolute bottom-2">
@@ -26,8 +27,8 @@
 </template>
 
 <script setup lang="ts">
-import { ServerIcon } from "@heroicons/vue/20/solid"
-import { CpuChipIcon } from "@heroicons/vue/24/outline"
+import {ServerIcon} from "@heroicons/vue/20/solid"
+import {CpuChipIcon} from "@heroicons/vue/24/outline"
 
 const props = defineProps<{
   info: ServerInfo
