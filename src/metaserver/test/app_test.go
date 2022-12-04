37514f433b83cd8fedf005333514920ea404ed1f
Shijh <a838239178@gmail.com>
2022-12-04 13:50:56 +0800

    fix: return pointer of config

--- src/metaserver/test/app_test.go
+++ src/metaserver/test/app_test.go
@@ -1,42 +1,31 @@
 package test
 
 import (
-	"common/datasize"
 	"metaserver/config"
 	"metaserver/internal/app"
 	"testing"
 )
 
-func TestUpdateConfig(t *testing.T) {
-	var cfg config.Config
-	cfg = config.ReadConfigFrom("../../../test_conf/meta-server-1.yaml")
-	cfg.Cache.MaxSize = 512 * datasize.MB
-	cfg.HashSlot.Slots = []string{"1-110", "2-224"}
-	if err := cfg.Persist(); err != nil {
-		panic(err)
-	}
-}
-
 func TestRunMeta1(t *testing.T) {
-	var cfg config.Config
+	var cfg *config.Config
 	cfg = config.ReadConfigFrom("../../../test_conf/meta-server-1.yaml")
-	app.Run(&cfg)
+	app.Run(cfg)
 }
 
 func TestRunMeta2(t *testing.T) {
-	var cfg config.Config
+	var cfg *config.Config
 	cfg = config.ReadConfigFrom("../../../test_conf/meta-server-2.yaml")
-	app.Run(&cfg)
+	app.Run(cfg)
 }
 
 func TestRunMeta3(t *testing.T) {
-	var cfg config.Config
+	var cfg *config.Config
 	cfg = config.ReadConfigFrom("../../../test_conf/meta-server-3.yaml")
-	app.Run(&cfg)
+	app.Run(cfg)
 }
 
 func TestRunMeta4(t *testing.T) {
-	var cfg config.Config
+	var cfg *config.Config
 	cfg = config.ReadConfigFrom("../../../test_conf/meta-server-4.yaml")
-	app.Run(&cfg)
+	app.Run(cfg)
 }
