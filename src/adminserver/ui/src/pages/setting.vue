db15ba2d6ff508381ce4202c778c864cc2016b3b
Shijh <a838239178@gmail.com>
2022-12-11 00:33:18 +0800

    feat(ui): setting page

--- src/adminserver/ui/src/pages/setting.vue
+++ src/adminserver/ui/src/pages/setting.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="flex items-baseline space-x-3">
-    <div class="btn-pri">
-      {{ t('link') }}
+  <div class="w-1/4 divide-y divide-gray-400 mx-10">
+    <!-- group1 -->
+    <div class="panel">
+      <div class="title">{{ t('general-setting') }}</div>
+      <div class="form-body">
+        <label>{{ t('locale-label') }}</label>
+        <PriListBox @change="setLocale" v-model="$i18n.locale" :options="$i18n.availableLocales" :format="t" class="col-span-2">
+        </PriListBox>
+      </div>
     </div>
-    <div class="flex items-baseline">
-      <label class="text-gray-900 text-sm mx-2">{{  t('locale-label')  }}</label>
-      <PriListBox @change="setLocale" v-model="$i18n.locale" :options="$i18n.availableLocales" :format="t" class="w-72">
-      </PriListBox>
+    <!-- group2 -->
+    <div class="panel">
+      <div class="title">{{ t('ui-setting') }}</div>
+      <div class="form-body">
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +30,24 @@ function setLocale(lang: string) {
 }
 </script>
 
+<style scoped>
+.panel {
+  @apply pt-6 pb-10 px-2
+}
+
+.panel .title {
+  @apply font-bold text-xl text-gray-900
+}
+
+.panel .form-body {
+  @apply grid grid-cols-3 items-center mt-4 gap-y-2
+}
+
+.panel .form-body label {
+  @apply text-gray-900 text-center
+}
+</style>
+
 <route lang="json">
 {
   "meta": {
@@ -34,12 +59,14 @@ function setLocale(lang: string) {
 
 <i18n lang="yaml">
 en:
-  link: 'linked'
-  locale-label: 'locale'
+  general-setting: 'General'
+  ui-setting: 'User Interface'
+  locale-label: 'Locale'
   en: 'English(US)'
   zh: 'Chinese-Simple'
 zh:
-  link: '链接'
+  general-setting: '常规'
+  ui-setting: '界面'
   locale-label: '语言'
   en: '英语(美国)'
   zh: '中文简体'
