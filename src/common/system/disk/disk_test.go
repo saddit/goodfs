74fa664513d28cb1d804dd0d4e0bc2054d21dbc4
Shijh <a838239178@gmail.com>
2023-01-07 16:29:32 +0800

    feat(common): partitions.go

--- src/common/system/disk/disk_test.go
+++ src/common/system/disk/disk_test.go
@@ -1,8 +1,13 @@
 package disk
 
 import (
+	"common/logs"
 	"testing"
 )
+
+func init() {
+	logs.SetLevel(logs.Debug)
+}
 
 func TestGetInfo(t *testing.T) {
 	info, err := GetInfo(Root)
@@ -10,4 +15,12 @@ func TestGetInfo(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("total=%dGB, free=%dGB", info.Total.GigaByte(), info.Total.GigaByte())
+}
+
+func TestAllPartitionPath(t *testing.T) {
+	paths, err := AllPartitionPath()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("%v", paths)
 }
