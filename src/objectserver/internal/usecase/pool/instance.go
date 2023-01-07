6821f35454b26de077843684ded34f3d9707327e
Shijh <a838239178@gmail.com>
2023-01-07 21:32:37 +0800

    feat: driver manager

--- src/objectserver/internal/usecase/pool/instance.go
+++ src/objectserver/internal/usecase/pool/instance.go
@@ -2,49 +2,67 @@ package pool
 
 import (
 	"common/cache"
+	"common/etcd"
 	"common/registry"
 	"common/util"
 	"objectserver/config"
 	"objectserver/internal/db"
+	"objectserver/internal/usecase/service"
 
 	"github.com/allegro/bigcache"
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 var (
-	Config    *config.Config
-	Cache     cache.ICache
-	Etcd      *clientv3.Client
-	Registry  registry.Registry
-	Discovery registry.Discovery
-	ObjectCap *db.ObjectCapacity
+	Config        *config.Config
+	Cache         cache.ICache
+	Etcd          *clientv3.Client
+	Registry      registry.Registry
+	Discovery     registry.Discovery
+	ObjectCap     *db.ObjectCapacity
+	DriverManager *service.DriverManager
 )
 
 func InitPool(cfg *config.Config) {
 	Config = cfg
-	//init cache
-	{
-		cacheConf := bigcache.DefaultConfig(cfg.Cache.TTL)
-		cacheConf.CleanWindow = cfg.Cache.CleanInterval
-		cacheConf.HardMaxCacheSize = int(cfg.Cache.MaxSize.MegaByte())
-		cacheConf.Shards = int(cfg.Cache.MaxSize / cfg.Cache.MaxItemSize)
-		Cache = cache.NewCache(cacheConf)
-	}
+	initDm()
+	initCache(&cfg.Cache)
+	initEtcd(&cfg.Etcd)
+	initRegister(Etcd, cfg)
+	initObjectCap(Etcd, cfg)
+}
 
+func initDm() {
+	DriverManager = service.NewDriverManager(service.NewFreeFirstDriver())
+}
+
+func initObjectCap(et *clientv3.Client, cfg *config.Config) {
+	ObjectCap = db.NewObjectCapacity(et, cfg)
+}
+
+func initCache(cfg *config.CacheConfig) {
+	cacheConf := bigcache.DefaultConfig(cfg.TTL)
+	cacheConf.CleanWindow = cfg.CleanInterval
+	cacheConf.HardMaxCacheSize = int(cfg.MaxSize.MegaByte())
+	cacheConf.Shards = int(cfg.MaxSize / cfg.MaxItemSize)
+	Cache = cache.NewCache(cacheConf)
+}
+
+func initEtcd(cfg *etcd.Config) {
 	var e error
 	if Etcd, e = clientv3.New(clientv3.Config{
-		Endpoints: cfg.Etcd.Endpoint,
-		Username:  cfg.Etcd.Username,
-		Password:  cfg.Etcd.Password,
+		Endpoints: cfg.Endpoint,
+		Username:  cfg.Username,
+		Password:  cfg.Password,
 	}); e != nil {
 		panic(e)
 	}
+}
 
-	ObjectCap = db.NewObjectCapacity(Etcd, cfg)
-	
+func initRegister(et *clientv3.Client, cfg *config.Config) {
 	cfg.Registry.HttpAddr = util.GetHostPort(cfg.Port)
 	cfg.Registry.RpcAddr = util.GetHostPort(cfg.RpcPort)
-	er := registry.NewEtcdRegistry(Etcd, cfg.Registry)
+	er := registry.NewEtcdRegistry(et, cfg.Registry)
 	Registry, Discovery = er, er
 }
 
