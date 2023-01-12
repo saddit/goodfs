12c8d57289d2cd9b5cdf92b3f68fc8e960fb2b6a
ShiJh⭐ <a838239178@gmail.com>
2023-01-12 06:02:39 +0000

    feat: WriteWithSize

--- src/objectserver/internal/usecase/service/object_test.go
+++ src/objectserver/internal/usecase/service/object_test.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"bytes"
+	"io"
+	"os"
 	"path/filepath"
 
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -43,4 +46,70 @@ func TestWriteReadDeleteFile(t *testing.T) {
 	}
 	t.Log(buf.String())
 	assert.New(t).Equal(4096+13, buf.Len())
+}
+
+func TestWriteWithSize(t *testing.T) {
+	defer func() {
+		_ , err := DeleteFile(".", "new_file")
+		if err != nil {
+			t.Error(err)
+			return	
+		}
+	}()
+	var bt []byte
+	var buffer *bytes.Buffer
+	fstSize, secSize := 8000, 4108
+	// write first time
+	bt = make([]byte, fstSize)
+	for i := range bt {
+		bt[i] = 'A'
+	}
+	buffer = bytes.NewBuffer(bt)
+	n, err := WriteFileWithSize("./new_file", 0, buffer)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("fist write %d bytes", n)
+	assert.New(t).GreaterOrEqual(n, int64(fstSize))
+	// write second time
+	bt = make([]byte, secSize)
+	for i := range bt {
+		bt[i] = 'B'
+	}
+	buffer = bytes.NewBuffer(bt)
+	n, err = WriteFileWithSize("./new_file", int64(fstSize), buffer)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	t.Logf("second write %d bytes", n)
+	assert.New(t).GreaterOrEqual(n, int64(secSize))
+	// read file
+	exceptSize := ((fstSize + secSize) - ((fstSize + secSize) % 4096)) + 4096
+	fi, err := os.Open("./new_file")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer fi.Close()
+	bt, err = io.ReadAll(fi)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	assert.New(t).Equal(exceptSize, len(bt))
+	var numOfA, numOfB, numOfOther int
+	for _, b := range bt[:fstSize+secSize] {
+		if b == 'A' {
+			numOfA++
+		} else if b == 'B' {
+			numOfB++
+		} else {
+			numOfOther++
+		}
+	}
+	assert.New(t).Equal(0, numOfOther)
+	assert.New(t).Equal(fstSize, numOfA)
+	assert.New(t).Equal(secSize, numOfB)
 }
