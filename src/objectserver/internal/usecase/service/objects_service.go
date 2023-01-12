ff195ac98dc7d20d430fe8c82a28557a1c11012b
Shijh <a838239178@gmail.com>
2023-01-12 20:08:29 +0800

    docs: comments

--- src/objectserver/internal/usecase/service/objects_service.go
+++ src/objectserver/internal/usecase/service/objects_service.go
@@ -18,6 +18,7 @@ const (
 	LocateKeyPrefix = "LocateCache#"
 )
 
+// Exist check if the object exists. pass to ExistPath
 func Exist(name string) bool {
 	if global.Cache.Has(LocateKeyPrefix+name) || global.Cache.Has(name) {
 		return true
@@ -30,19 +31,23 @@ func Exist(name string) bool {
 	}
 }
 
+// ExistPath check if the given path exists.
 func ExistPath(fullPath string) bool {
 	_, err := os.Stat(fullPath)
-	return os.IsExist(err) || !os.IsNotExist(err)
+	return !os.IsNotExist(err)
 }
 
+// MarkExist save object mark into cache
 func MarkExist(name string) {
 	global.Cache.Set(LocateKeyPrefix+name, []byte{})
 }
 
+// UnMarkExist remove object mark from cache
 func UnMarkExist(name string) {
 	global.Cache.Delete(LocateKeyPrefix + name)
 }
 
+// Put save object to storage path
 func Put(fileName string, fileStream io.Reader) error {
 	if Exist(fileName) {
 		return nil
@@ -59,6 +64,7 @@ func Put(fileName string, fileStream io.Reader) error {
 	return nil
 }
 
+// Get read object to writer with provided size. pass to GetFile
 func Get(name string, offset, size int64, writer io.Writer) error {
 	if err := GetFile(filepath.Join(global.Config.StoragePath, name), offset, size, writer); err != nil {
 		return err
@@ -67,10 +73,13 @@ func Get(name string, offset, size int64, writer io.Writer) error {
 	return nil
 }
 
+// GetTemp read temp file to writer with provided size. pass to GetFile
 func GetTemp(name string, size int64, writer io.Writer) error {
 	return GetFile(filepath.Join(global.Config.TempPath, name), 0, size, writer)
 }
 
+// GetFile read file with provided size. offset corresponds to io.SeekStart.
+// if offset is not multiple of 4KB, direct-io will be disabled
 func GetFile(fullPath string, offset, size int64, writer io.Writer) error {
 	f, err := disk.OpenFileDirectIO(fullPath, os.O_RDONLY, cst.OS.ModeUser)
 	if os.IsNotExist(err) {
@@ -82,7 +91,7 @@ func GetFile(fullPath string, offset, size int64, writer io.Writer) error {
 	defer f.Close()
 	if offset > 0 {
 		if int(offset)%cst.OS.PageSize > 0 {
-			logs.Std().Warn("offset must be power of 4KB, direct-io will disabled")
+			logs.Std().Warn("offset must be power of 4KB, direct-io will be disabled")
 			if err = disk.DisableDirectIO(f); err != nil {
 				return fmt.Errorf("diable direct-io: %w", err)
 			}
@@ -97,6 +106,7 @@ func GetFile(fullPath string, offset, size int64, writer io.Writer) error {
 	return nil
 }
 
+// Delete remove the object under the storage path
 func Delete(name string) error {
 	size, err := DeleteFile(global.Config.StoragePath, name)
 	if err != nil {
@@ -110,6 +120,7 @@ func Delete(name string) error {
 	return nil
 }
 
+// DeleteFile will remove the file under the path if file not exist, do nothing
 func DeleteFile(path, name string) (int64, error) {
 	pt := filepath.Join(path, name)
 	info, err := os.Stat(path)
@@ -126,7 +137,7 @@ func DeleteFile(path, name string) (int64, error) {
 }
 
 // WriteFileWithSize will use provided curSize to remove padding of tail
-// and keep writing data aligened to multiple of 4KB
+// and keep writing data aligned to multiple of 4KB
 func WriteFileWithSize(fullPath string, curSize int64, fileStream io.Reader) (int64, error) {
 	file, err := disk.OpenFileDirectIO(fullPath, os.O_RDWR|os.O_CREATE, cst.OS.ModeUser)
 	if err != nil {
@@ -157,7 +168,7 @@ func WriteFileWithSize(fullPath string, curSize int64, fileStream io.Reader) (in
 			return 0, fmt.Errorf("read tail except %d but %d", pageSize, n)
 		}
 		// remove padding
-		bt = bt[:len(bt) - int(paddingLen)]
+		bt = bt[:len(bt)-int(paddingLen)]
 		// concatenation with fileStream
 		fileStream = disk.MultiReader(bytes.NewBuffer(bt), fileStream)
 		// seek back
@@ -165,21 +176,22 @@ func WriteFileWithSize(fullPath string, curSize int64, fileStream io.Reader) (in
 			return 0, err
 		}
 	}
-	// write file and aligen to power of 4KB
-	return disk.NewAligendWriter(file).ReadFrom(fileStream)
+	// write file and aligned to power of 4KB
+	return disk.NewAlignedWriter(file).ReadFrom(fileStream)
 }
 
-// WriteFile should make sure size of each write is a multiple of 4096 (except last) 
+// WriteFile should make sure size of each write is a multiple of 4096 (except last)
 func WriteFile(fullPath string, fileStream io.Reader) (int64, error) {
 	file, err := disk.OpenFileDirectIO(fullPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, cst.OS.ModeUser)
 	if err != nil {
 		return 0, err
 	}
 	defer file.Close()
-	// write file and aligen to power of 4KB
-	return disk.NewAligendWriter(file).ReadFrom(fileStream)
+	// write file and aligned to power of 4KB
+	return disk.NewAlignedWriter(file).ReadFrom(fileStream)
 }
 
+// MvTmpToStorage move the temp file to storage path with a new name
 func MvTmpToStorage(tmpName, fileName string) error {
 	filePath := filepath.Join(global.Config.StoragePath, fileName)
 	tempPath := filepath.Join(global.Config.TempPath, tmpName)
