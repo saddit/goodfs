3e6038d817d7c0ed18701514831cebe13e7dd988
Shijh <a838239178@gmail.com>
2023-01-13 23:31:03 +0800

    feat: etcd store for performance collector

--- src/common/performance/config.go
+++ src/common/performance/config.go
@@ -11,9 +11,9 @@ const (
 )
 
 type Config struct {
-	Enable          bool          `yaml:"enable" env:"ENABLE" env-default:"false"`              // Enable should enable performance info collection. default is false
-	Store           StoreType     `yaml:"store" env:"STORE" env-default:"none"`                 // Store specify a storage type. default is none (not store anything)
-	SaveInterval    time.Duration `yaml:"save-interval" env:"SAVE_INTERVAL" env-default:"5m"`   // SaveInterval the interval to put in-memory records to store. it will be reset by other put behavior. default is 5 minute
-	MaxInMemory     int           `yaml:"max-in-memory" env:"MAX_IN_MEMORY" env-default:"1000"` // MaxInMemory is maximum records allowed to stay in memory, default is 1000.
-	SaveWhenReached bool          `yaml:"save-when-reached" env-default:"false"`                // SaveWhenReached saving immediately when mem records reach max size if true. otherwise the first in will first out. default is false.
+	Enable           bool          `yaml:"enable" env:"ENABLE" env-default:"false"`                         // Enable should enable performance info collection. default is false
+	Store            StoreType     `yaml:"store" env:"STORE" env-default:"none"`                            // Store specify a storage type. default is none (not store anything)
+	FlushInterval    time.Duration `yaml:"flush-interval" env:"FLUSH_INTERVAL" env-default:"5m"`            // FlushInterval the interval to flush in-memory records to Store. it will be reset by FlushWhenReached. default is 5 minute.
+	MaxInMemory      int           `yaml:"max-in-memory" env:"MAX_IN_MEMORY" env-default:"1000"`            // MaxInMemory is maximum records allowed to stay in memory, default is 1000.
+	FlushWhenReached bool          `yaml:"flush-when-reached" env:"FLUSH_WHEN_REACHED" env-default:"false"` // FlushWhenReached flush immediately when in-memory size reach limitation if true. otherwise the first in will first out. default is false.
 }
